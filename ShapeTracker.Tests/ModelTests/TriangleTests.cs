8b7bc84091f3b2a5131101a189b310708bc08fef
Eliot Gronstal <eliot.lauren@gmail.com>
2023-02-19 11:47:23 -0800

    add Dispose method to clean up between each test and complete testing.

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -1,11 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShapeTracker.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ShapeTracker.Tests
 {
   [TestClass]
-  public class TriangleTests
+  public class TriangleTests : IDisposable
   {
+    public void Dispose()
+    {
+      Triangle.ClearAll();
+    }
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
@@ -74,6 +80,72 @@ namespace ShapeTracker.Tests
       newTriangle.SetSide3(newLength3);
       // Assert
       Assert.AreEqual(newLength3, newTriangle.GetSide3());
+    }
+        [TestMethod]
+    public void CheckType_DeterminesWhenNotATriangle_String()
+    {
+      // Arrange
+      Triangle notATriangle = new Triangle(3, 4, 88);
+      // Act
+      string triType = notATriangle.CheckType();
+      // Assert
+      Assert.AreEqual("not a triangle", triType);
+    }
+    [TestMethod]
+    public void CheckType_DeterminesWhenScalene_String()
+    {
+      // Arrange
+      Triangle scaleneTri = new Triangle(3, 4, 5);
+      // Act
+      string triType = scaleneTri.CheckType();
+      // Assert
+      Assert.AreEqual("scalene triangle", triType);
+    }
+    [TestMethod]
+    public void CheckType_DeterminesWhenIsosceles_String()
+    {
+      // Arrange
+      Triangle isoscelesTri = new Triangle(5, 4, 4);
+      // Act
+      string triType = isoscelesTri.CheckType();
+      // Assert
+      Assert.AreEqual("isosceles triangle", triType);
+    }
+    [TestMethod]
+    public void CheckType_DeterminesWhenEquilateral_String()
+    {
+      // Arrange
+      Triangle equiTri = new Triangle(4, 4, 4);
+      // Act
+      string triType = equiTri.CheckType();
+      // Assert
+      Assert.AreEqual("equilateral triangle", triType);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsAllTriangleInstances_List()
+    {
+      // Arrange
+      Triangle tri1 = new Triangle(2, 2, 9);
+      Triangle tri2 = new Triangle(21, 3, 9);
+      Triangle tri3 = new Triangle(1, 3, 9);
+      List<Triangle> expected = new List<Triangle> { tri1, tri2, tri3 };
+      // Act
+      List<Triangle> actualResult = Triangle.GetAll();
+      // Assert
+      CollectionAssert.AreEqual(expected, actualResult);
+    }
+    [TestMethod]
+    public void ClearAll_DeletesAllTriangleInList_Void()
+    {
+      // Arrange
+      Triangle tri1 = new Triangle(2, 2, 9);
+      Triangle tri2 = new Triangle(21, 3, 9);
+      Triangle tri3 = new Triangle(1, 3, 9);
+      List<Triangle> expected = new List<Triangle> { };
+      // Act
+      Triangle.ClearAll();
+      // Assert
+      CollectionAssert.AreEqual(expected, Triangle.GetAll());
     }
   }
 }
